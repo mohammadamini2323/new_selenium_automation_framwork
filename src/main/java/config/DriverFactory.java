97eae22fa9235d090bb6763413000d02523a133e
Mhammad <moh.amini2323@gmail.com>
2024-12-05 19:59:47 -0500

    DF

--- src/main/java/config/DriverFactory.java
+++ src/main/java/config/DriverFactory.java
@@ -1,10 +1,14 @@
 package config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.EdgeDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import static io.opentelemetry.semconv.SemanticAttributes.NetHostConnectionSubtypeValues.EDGE;
 
@@ -12,24 +16,41 @@ public class DriverFactory {
     public static WebDriver driver;
     public static WebDriver initializeWebDriver(){
         String browser =ConfigManager.getProperty("browser").toUpperCase();
+        boolean isHeadless=Boolean.parseBoolean(ConfigManager.getProperty("isHeadless"));
         switch (browser) {
-            case CHROME:
-               // WebDriverManager.chromedriver().setup();
-             driver=new ChromeDriver();
+            case "CHROME":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions=new ChromeOptions();
+                if (isHeadless){
+                chromeOptions.addArguments("--headless");}
+             driver=new ChromeDriver(chromeOptions);
              break;
 
-            case EDGE:
-               driver=new EdgeDriver();
+            case "EDGE":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions=new EdgeOptions();
+                if (isHeadless){
+                    edgeOptions.addArguments("--headless");}
+               driver=new EdgeDriver(edgeOptions);
                break;
-            case FIREFOX:
-                driver= new FirefoxDriver();
+            case "FIREFOX":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions=new FirefoxOptions();
+                if (isHeadless){
+                    firefoxOptions.addArguments("--headless");}
+                driver= new FirefoxDriver(firefoxOptions);
             default:
                 System.out.println("invalid input");
 
 
         }
+        driver.manage().window().maximize();
+
         return driver;
 
+    }
+    public static void quitDriver(){
+        driver.quit();
     }
 
 
